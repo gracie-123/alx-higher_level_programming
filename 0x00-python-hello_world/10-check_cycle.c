8f6eb6d3601105359d05b29e5aeed9d136a057e5
gracie-123 <gracietumbo@gmail.com>
2023-11-01 12:46:04 -0700

    0x00. Python - Hello, World

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,31 +1,88 @@
+#!/usr/bin/python3
+void findloop(struct node *head) {
+    struct node *slow, *fast;
+    slow = fast = head;
+   
+    while(slow && fast && fast->next) {
+        /* Slow pointer will move one node per iteration whereas 
+        fast node will move two nodes per iteration */
+        slow = slow->next;
+        fast  = fast->next->next;
+        if (slow == fast) {
+           printf("Linked List contains a loop\n");
+           return;
+        }
+    }
+    printf("No Loop in Linked List\n");
+}
+
+#include <stdio.h>
 #include <stdlib.h>
-#include "lists.h"
-
-/**
-* check_cycle - checks if a singly linked list contains a cycle
-* nlist: a singly-linked list
-
-* Return:if there is no cycle 0
-          if there is a cycle 1
+  
+/* A structure of linked list node */
+struct node {
+  int data;
+  struct node *next;
+} *head;
+ 
+void initialize(){
+    head = NULL;
+}
+ 
+/* 
+Given a Inserts a node in front of a singly linked list. 
 */
-int check_cycle(listint_t *list);
-{
-          listint_t *slow, *fast;
-
-          if (list == NULL || list->next == NULL)
-                  return (0);
-
-          slow = list->next;
-          fast = list->next->next;
-
-          while (slow && fast &&fast-.next)
-          {
-          if (slow == fast)
-                   return (1);
-
-          slow = slow->next;
-          fast = fast->next->next;
-          }
-
-          return (0);
+void insert(int num) {
+    /* Create a new Linked List node */
+    struct node* newNode = (struct node*) malloc(sizeof(struct node));
+    newNode->data  = num;
+    /* Next pointer of new node will point to head node of linked list  */
+    newNode->next = head;
+    /* make new node as new head of linked list */
+    head = newNode;
+    printf("Inserted Element : %d\n", num);
+}
+ 
+void findloop(struct node *head) {
+    struct node *slow, *fast;
+    slow = fast = head;
+   
+    while(slow && fast && fast->next) {
+        /* Slow pointer will move one node per iteration whereas 
+        fast node will move two nodes per iteration */
+        slow = slow->next;
+        fast  = fast->next->next;
+        if (slow == fast) {
+           printf("Linked List contains a loop\n");
+           return;
+        }
+    }
+    printf("No Loop in Linked List\n");
+}
+/*
+ Prints a linked list from head node till tail node 
+*/
+void printLinkedList(struct node *nodePtr) {
+  while (nodePtr != NULL) {
+     printf("%d", nodePtr->data);
+     nodePtr = nodePtr->next;
+     if(nodePtr != NULL)
+         printf("-->");
+  }
+}
+  
+int main() {
+    initialize();
+    /* Creating a linked List*/
+    insert(8);  
+    insert(3); 
+    insert(2); 
+    insert(7);
+    insert(9);
+     
+    /* Create loop in linked list. Set next pointer of last node to second node from head */
+    head->next->next->next->next->next = head->next;
+ 
+    findloop(head);
+    return 0;
 }
