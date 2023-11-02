cd9516b83fbb0d1d05d19c39e8566f855791b02f
gracie-123 <gracietumbo@gmail.com>
2023-11-02 12:04:31 -0700

    0x02. Python

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import hidden_4
 if __name__ == "__main__":
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)ii
+    import hidden_4
+    dir(hidden_4)
+    for i in dir(hidden_4):
+        if i[0:2] != "__":
+            print(i)
