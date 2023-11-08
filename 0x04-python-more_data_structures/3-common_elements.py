eca8ea9409edbbaa09e7f569c58bf29230af3b8b
gracie-123 <gracietumbo@gmail.com>
2023-11-08 11:19:56 -0800

    0x04. Python - More Data Structures: Set, Dictionary

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    """
+    function returns a set of common elements in two sets.
+    """
+    common = set_1 & set_2
+
+    return (common)
