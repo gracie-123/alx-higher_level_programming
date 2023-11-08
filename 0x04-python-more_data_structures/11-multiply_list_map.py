f2620a5c498380ce49dba1da9f9a819abd5771c3
gracie-123 <gracietumbo@gmail.com>
2023-11-08 11:38:10 -0800

    0x04. Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
 def multiply_list_map(my_list=[], number=0):
-    mul = list(map(lambda number:number * number, my_list))
-    return mul
+    return (list(map(lambda x: x*number, my_list)))
