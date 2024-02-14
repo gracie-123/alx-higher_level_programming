b2dc94df0abbc9efa91e59fc5f4e32ea8842a1f0
gracie-123 <gracietumbo@gmail.com>
2024-02-14 10:01:16 -0800

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-// A script that prints 3 lines about C, Python and JS
-
-const myVar1 = 'C is fun';
-const myVar2 = 'Python is cool';
-const myVar3 = 'Javascript is amazing';
-
-console.log(myVar1 + '\n' + myVar2 + '\n' + myVar3);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
